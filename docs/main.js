eaafe1c338a50cde60a035a0abd4b3ebff7a311e
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2022-03-13 17:58:48 +0100

    player movement

--- docs/main.js
+++ docs/main.js
@@ -22,12 +22,51 @@ window.addEventListener('resize', ()=>{
 	app.renderer.resize(_W, _H)
 })
 
-const playerTexture = PIXI.Texture.from("assets/knight.png")
+let speed = 3
+
+const playerTexture = PIXI.Texture.from("assets/player.png")
 const player = new PIXI.Sprite(playerTexture)
 
-player.width = 100
-player.height = 100
+player.width = 60
+player.height = 60
 player.anchor.set(0.5)
 player.position.set(_W/2, _H/2)
 
 app.stage.addChild(player)
+
+const keys = {}
+const events = {
+	ArrowUp(){
+		player.y -= speed
+	},
+	ArrowDown(){
+		player.y += speed
+	},
+	ArrowLeft(){
+		player.x -= speed
+		player.scale.x *= Math.abs(player.scale.x)/player.scale.x*-1
+	},
+	ArrowRight(){
+		player.x += speed
+		player.scale.x *= Math.abs(player.scale.x)/player.scale.x
+	}
+}
+
+window.addEventListener('keydown', (e)=>{
+	keys[e.code] = true
+})
+window.addEventListener('keyup', (e)=>{
+	keys[e.code] = null
+})
+
+const loop =()=>{
+	requestAnimationFrame(loop)
+	for(const i in events){
+		if(keys[i]){
+			events[i]()
+		}
+	}
+}
+
+loop()
+
